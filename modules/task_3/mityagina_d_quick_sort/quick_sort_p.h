672b378ff657bb0c651e02f350edc6fbb6c28bf6
DariaMityagina <daria.mityagina@intel.com>
2020-01-12 11:10:38 +0300

    Fixes

--- modules/task_3/mityagina_d_quick_sort/quick_sort_p.h
+++ modules/task_3/mityagina_d_quick_sort/quick_sort_p.h
@@ -1,16 +1,16 @@
-// Copyright 2019 Mityagina Daria
-#ifndef MODULES_TASK_3_MITYAGINA_D_QUICK_SORT_QUICK_SORT_P_H_
-#define MODULES_TASK_3_MITYAGINA_D_QUICK_SORT_QUICK_SORT_P_H_
+// Copyright 2019-2020 Mityagina Daria
+#ifndef MODULES_TASK_3_ANOTHER_WAY_OF_THIRD_LAB_IMPLEMENTATION_QUICK_SORT_P_H_
+#define MODULES_TASK_3_ANOTHER_WAY_OF_THIRD_LAB_IMPLEMENTATION_QUICK_SORT_P_H_
 #include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
 
-std::vector<int> getRandomVector(int vectorSize);
-std::vector<int> quick_s(std::vector<int> _vector, int left, int right);
-std::vector<int> Merge_my_vectors(std::vector<int> my_vector1, std::vector<int> my_vector2, int m, int n);
-std::vector<int> main_work(std::vector<int> my_vector, int N);
-int part(std::vector<int> _vector, int left, int right);
-std::vector<int> part_vect(std::vector<int> _vector, int left, int right);
+std::vector<int> getRandomVector(int size);
+void merge(std::vector<int> array1, std::vector<int> array2, std::vector<int> *merged_array, int size);
+int partition(std::vector<int> *array, int start, int end);
+void quickSort(std::vector<int> *array, int start, int end);
+void quickSort(std::vector<int> *array, int size);
+void main_work(std::vector<int> *A, int size);
 
-#endif  // MODULES_TASK_3_MITYAGINA_D_QUICK_SORT_QUICK_SORT_P_H_
+#endif  // MODULES_TASK_3_ANOTHER_WAY_OF_THIRD_LAB_IMPLEMENTATION_QUICK_SORT_P_H_
